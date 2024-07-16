153ea713e54189bcb239883f9018ef999a033e48
fronceee <tharathip.tamwiset@gmail.com>
2024-07-17 01:28:49 +0700

    add crawlPage to main.js

--- crawl.js
+++ crawl.js
@@ -1,11 +1,11 @@
 import { JSDOM } from "jsdom";
 
-function normalizeURL(url) {
+export function normalizeURL(url) {
   const urlObject = new URL(url);
   return urlObject.hostname + urlObject.pathname;
 }
 
-function getURLsFromHTML(htmlBody, baseURL) {
+export function getURLsFromHTML(htmlBody, baseURL) {
   const dom = new JSDOM(htmlBody);
   const allUrls = dom.window.document.querySelectorAll("a");
   const urls = [];
@@ -19,21 +19,52 @@ function getURLsFromHTML(htmlBody, baseURL) {
   return urls;
 }
 
-async function crawlPage(baseURL) {
+async function crawl(baseURL) {
   try {
     fetch(baseURL).then(async (res) => {
       if (res.status >= 400) {
-        console.error("error fetching", baseURL, res.status);
-        return;
+        throw new Error(`error fetching ${baseURL} ${res.status}`);
       }
-      if (!res.headers.get("content-type").includes("text/html")) {
-        console.log("not an html page", baseURL);
-        return;
+      const contentType = res.headers.get("content-type");
+      if (!contentType || !contentType.includes("text/html")) {
+        throw new Error(`not an html page ${baseURL}`);
       }
       console.log("fetching", baseURL);
-      console.log(await res.text());
+      return res.text();
     });
   } catch (error) {
-    console.error(error);
+    console.log(error.message);
   }
+}
+
+export async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+  if (!isSameDomain(baseURL, currentURL)) {
+    return pages;
+  }
+  const cleanedUrl = normalizeURL(baseURL);
+  if (pages[cleanedUrl]) {
+    pages[cleanedUrl]++;
+    return pages;
+  } else {
+    pages[cleanedUrl] = 1;
+  }
+
+  let html = "";
+  try {
+    html = await crawl(baseURL);
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  const urls = getURLsFromHTML(html, baseURL);
+  for (const url of urls) {
+    pages = await crawlPage(baseURL, url, pages);
+  }
+  return pages;
+}
+
+function isSameDomain(url1, url2) {
+  const url1Object = new URL(url1);
+  const url2Object = new URL(url2);
+  return url1Object.hostname === url2Object.hostname;
 }
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function main() {
+import { crawlPage } from "./crawl.js";
+
+async function main() {
   if (process.argv.length < 3) {
     console.log("no url provided");
   } else if (process.argv.length > 3) {
@@ -6,6 +8,8 @@ function main() {
   } else {
     const url = process.argv[2];
     console.log("starting crawl for", url);
+    const crawlPages = await crawlPage(url);
+    console.log("crawl complete", crawlPages);
   }
 }
 main();
