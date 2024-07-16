c048084dbdc783ac2a0249d66c16b841e8a5097d
fronceee <tharathip.tamwiset@gmail.com>
2024-07-17 01:01:44 +0700

    add crawlPage function and init main function

--- crawl.js
+++ crawl.js
@@ -1,11 +1,11 @@
 import { JSDOM } from "jsdom";
 
-export function normalizeURL(url) {
+function normalizeURL(url) {
   const urlObject = new URL(url);
   return urlObject.hostname + urlObject.pathname;
 }
 
-export function getURLsFromHTML(htmlBody, baseURL) {
+function getURLsFromHTML(htmlBody, baseURL) {
   const dom = new JSDOM(htmlBody);
   const allUrls = dom.window.document.querySelectorAll("a");
   const urls = [];
@@ -17,4 +17,23 @@ export function getURLsFromHTML(htmlBody, baseURL) {
     }
   }
   return urls;
+}
+
+async function crawlPage(baseURL) {
+  try {
+    fetch(baseURL).then(async (res) => {
+      if (res.status >= 400) {
+        console.error("error fetching", baseURL, res.status);
+        return;
+      }
+      if (!res.headers.get("content-type").includes("text/html")) {
+        console.log("not an html page", baseURL);
+        return;
+      }
+      console.log("fetching", baseURL);
+      console.log(await res.text());
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no url provided");
+  } else if (process.argv.length > 3) {
+    console.log("too many arguments");
+  } else {
+    const url = process.argv[2];
+    console.log("starting crawl for", url);
+  }
+}
+main();
